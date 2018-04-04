6e78a23b29040e2f531ea4a817c91b5f2d99eafc
Jerzy Ptak <jerzy.ptak@bbitg.co.uk>
2018-04-04 17:33:50 +0100

    fixing memory leaks

--- daemon/codec.c
+++ daemon/codec.c
@@ -745,6 +745,7 @@ static int handler_func_transcode(struct codec_handler *h, struct call_media *me
 	if (packet_sequencer_insert(&ch->sequencer, &packet->p)) {
 		// dupe
 		mutex_unlock(&ch->lock);
+		obj_put(&ch->h);
 		__transcode_packet_free(packet);
 		ilog(LOG_DEBUG, "Ignoring duplicate RTP packet");
 		atomic64_inc(&mp->ssrc_in->duplicates);
